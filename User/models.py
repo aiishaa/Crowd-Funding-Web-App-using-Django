5b4701414821764677d82792c23d00184ac2c3b9
unknown <aisha.fathy123@gmail.com>
2024-03-25 10:20:18 +0200

    added the edit profile view and form

--- User/models.py
+++ User/models.py
@@ -15,18 +15,3 @@ class CustomForm(AbstractUser):
     @property
     def image_url(self):
         return f'/media/{self.profile_picture}'
-
-# class CustomEditForm(AbstractUser):
-#     phone = models.CharField(max_length=50, unique=True)
-#     profile_picture = models.ImageField(upload_to='user/profile_pictures/', blank=True)
-#     birthdate = models.DateField(null=True)
-#     facebook_profile = models.URLField(null=True)
-#     country = models.CharField(max_length=30)
-
-#     def __str__(self):
-#         return self.username
-    
-#     @property
-#     def image_url(self):
-#         return f'/media/{self.profile_picture}'
-
